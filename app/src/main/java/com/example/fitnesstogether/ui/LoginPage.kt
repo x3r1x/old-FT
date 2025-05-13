6d7e5578253cdea1fd87362bbe40ffe6553aa4c6
Gleb Ryzhov <glebka.r@yandex.ru>
2025-05-14 02:10:55 +0300

    добавил кнопку регистрации

--- app/src/main/java/com/example/fitnesstogether/ui/LoginPage.kt
+++ app/src/main/java/com/example/fitnesstogether/ui/LoginPage.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.example.fitnesstogether.R
 
 class LoginPage : Fragment() {
@@ -15,6 +19,14 @@ class LoginPage : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.login_screen, container, false)
+
+        view.findViewById<Button>(R.id.Next).setOnClickListener {
+            Toast.makeText(context, "Click!", Toast.LENGTH_SHORT).show()
+        }
+
+        view.findViewById<TextView>(R.id.registerButton).setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_loginPage_to_registerPage1)
+        }
 
         return view
     }
